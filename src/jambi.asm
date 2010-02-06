73f3e6094d1a6505a06fa7aa3a13fb94f268c149
sundas.christophe <sundas.christophe@a6361676-119f-11df-a992-f145ad325602>
2010-02-06 20:42:35 +0000

    :D

--- src/jambi.asm
+++ src/jambi.asm
@@ -20,9 +20,9 @@ includelib c:\masm32\lib\msvcrt.lib
 .data
 
 ;FilePattern seeked ;dans un premier temps*.exe mais checker le header serait mieux
-lpFileName db	"*.*", 0
+lpFileName db	"*.exe", 0
 
-sPutInt db "EAX: %i",13,10, 0
+varPutInt db "EAX: %i",13,10, 0
 sMsg db "File : %s, size : %i", 13,10,0
 varNum dd 45  
 varString db "HeyLow World " ,13,10, 0
@@ -31,10 +31,10 @@ varString db "HeyLow World " ,13,10, 0
 
 ;
 lpFindFileData WIN32_FIND_DATA <>	
-iSize DWORD ?
+lowSize dd ?
 hFile HANDLE ?
 hFile2 HANDLE ?
-
+highSize dd ?
 .code
 	
 ;offset globales || addr locales
@@ -58,18 +58,18 @@ nextFile:
 ; open the file RW Mode
 ; hFile2 = CreateFileA(lpFindFileData.cFileName, GENERIC_READ|GENERIC_WRITE, 0, 0, OPEN_EXISTING, 0, 0)
 
-push	0
-push	0
-push	OPEN_EXISTING 
-push	0
-push	0
-mov		eax, GENERIC_WRITE
-or		eax, GENERIC_READ
-lea		eax, lpFindFileData.cFileName
-push	eax
-call		CreateFile
+; push	0
+; push	0
+; push	OPEN_EXISTING 
+; push	0
+; push	0
+; mov	eax, GENERIC_WRITE
+; or		eax, GENERIC_READ
+; lea		eax, lpFindFileData.cFileName
+; push	eax
+; call		CreateFile
 
-mov		hFile2, eax
+; mov		hFile2, eax
 
 ; if (hFile2 == INVALID_HANDLE_VALUE)
 ; cmp hFile2, INVALID_HANDLE_VALUE
@@ -78,25 +78,44 @@ mov		hFile2, eax
 ; open the file ->penser a close et comparer avec invalide handle
 
 ; recup la taille du fichier
-; isSize = GetFileSize(hFile2, 0)
-push	0
-lea		eax, hFile2
-push	eax
-call		GetFileSize
-mov		iSize, eax
+; isSize = GetFileSize(hFile2, &highSize)
 
-;invoke	crt_printf, varString, iSize
+; lea		eax, highSize
+; push	eax
+;lea		eax, hFile2
+; push	hFile
+; call		GetFileSize
+; mov	lowSize, eax
+
+        ; Opens file for both read and write                                                                                                                                                                   
+        mov             eax, GENERIC_WRITE
+        or              eax, GENERIC_READ
+        invoke  CreateFile, addr lpFindFileData.cFileName, eax, 0, NULL, OPEN_EXISTING, 0, NULL
+        ; cmp             eax, INVALID_HANDLE_VALUE
+        ; je              endInfect
+        mov             hFile2, eax                                                                                                                                                                             
+
+        ; Checks file size                                                                                                                                                                                     
+        invoke  GetFileSize, hFile2,  NULL
+	mov lowSize, eax
+
+
+
 push	hFile2
 call		CloseHandle
 
+invoke	crt_printf, addr sMsg, addr lpFindFileData.cFileName, lowSize
+
+
+
 ; output FIleName Size
-; invoke crt_printf, addr sMsg, addr lpFindFileData.cFileName, iSizert
-push	iSize
-lea		eax, lpFindFileData.cFileName
-push	eax
-lea		eax, sMsg
-push	eax
-call		crt_printf
+;invoke	crt_printf, addr sMsg, addr lpFindFileData.cFileName, iSizert
+; push	iSize
+; lea		eax, lpFindFileData.cFileName
+; push	eax
+; lea		eax, sMsg
+; push	eax
+; call		crt_printf
 
 invoke FindNextFile,  hFile,  offset lpFindFileData
 cmp eax, 0
